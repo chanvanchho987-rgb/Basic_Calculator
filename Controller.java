2f92354c6fac586af14a92d08f9d04b480a821c2
sva <chanvanchho987@gmail.com>
2026-06-12 01:19:22 +0700

    u

---
+++ Controller.java
@@ -0,0 +1,70 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+
+    @FXML
+    private Button button_add;
+
+    @FXML
+    private Button button_backspace;
+
+    @FXML
+    private Button button_clear;
+
+    @FXML
+    private Button button_division;
+
+    @FXML
+    private Button button_eight;
+
+    @FXML
+    private Button button_equal;
+
+    @FXML
+    private Button button_five;
+
+    @FXML
+    private Button button_four;
+
+    @FXML
+    private Button button_minus;
+
+    @FXML
+    private Button button_multiplication;
+
+    @FXML
+    private Button button_nine;
+
+    @FXML
+    private Button button_one;
+
+    @FXML
+    private Button button_point;
+
+    @FXML
+    private Button button_seven;
+
+    @FXML
+    private Button button_six;
+
+    @FXML
+    private Button button_tree;
+
+    @FXML
+    private Button button_two;
+
+    @FXML
+    private Button button_zero;
+
+    @FXML
+    private Label label_result;
+
+    @FXML
+    void show_result(MouseEvent event) {
+
+    }
+
+}
