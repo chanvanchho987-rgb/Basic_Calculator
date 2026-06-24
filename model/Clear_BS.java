55e268695cc061f09c944e6e0f60ab804c37e0b1
Jasonop89 <sussybakagonnamakemeactup123@gmail.com>
2026-06-24 16:45:56 +0700

    aaa

--- model/Clear_BS.java
+++ model/Clear_BS.java
@@ -1,5 +1,27 @@
 package model;
 
 public class Clear_BS {
+    String var;
+
+    void clear() {
+        var = "";
+    }
+
+    void backspace() {
+        var = var.substring(0, var.length() - 1);
+    }
+
+    String get_var() {
+        return var;
+    }
+
+    public static void main(String[] args) {
+        Clear_BS clear_bs = new Clear_BS();
+        clear_bs.var = "12345";
+        clear_bs.clear();
+        System.out.println(clear_bs.get_var());
+        clear_bs.backspace();
+        System.out.println(clear_bs.get_var());
+    }
     
 }
--- model/Validation.java
+++ model/Validation.java
@@ -1,5 +1,44 @@
 package model;
 
 public class Validation {
-    
+    public boolean is_valid(String var) {
+        int count_dot = 0;
+        String new_var = "";
+        for (int i = 0; i < var.length(); i++) {
+            if (var.charAt(i) == '.') {
+                count_dot++;
+            }
+            new_var += var.charAt(i);
+        }
+        if(count_dot > 1){
+            return false;
+        }
+        return true;
+    }
+    public boolean is_valid_operand(String operator) {
+        if (operator.length() > 1) {
+            return false;
+        }
+        if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
+            return true;
+        }
+        return false;
+    }
+
+    String format_var(String var) {
+        return var;
+    }
+
+
+    String format_operator(String operator) {
+        return operator;
+    }
+
+    public static void main(String[] args) {
+        Validation validation = new Validation();
+        System.out.println(validation.is_valid("123.45"));
+        System.out.println(validation.is_valid("123.45.67"));
+        System.out.println(validation.is_valid_operand("+"));
+        System.out.println(validation.is_valid_operand("++"));
+    }
 }
