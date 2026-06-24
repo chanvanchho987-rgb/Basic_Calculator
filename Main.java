d141f4d68a524790f6968c5e71fc1cb5c371245e
sva <chanvanchho987@gmail.com>
2026-06-24 15:35:05 +0700

    u

--- Main.java
+++ Main.java
@@ -1,1 +1,32 @@
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        var view_main = getClass().getResource("View.fxml");
+
+        var loader = new FXMLLoader();
+        loader.setLocation(view_main);
+
+        var scene = new Scene(loader.load());
+
+        Image icon = new Image(getClass().getResourceAsStream("asset/Logo-ITC-PNG.png"));
+        stage.getIcons().add(icon);
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
