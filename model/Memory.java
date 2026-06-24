05790795077b06a986965a149bbed0762199ddb4
sva <chanvanchho987@gmail.com>
2026-06-24 15:48:08 +0700

    u

--- model/Memory.java
+++ model/Memory.java
@@ -1,5 +1,41 @@
 package model;
 
 public class Memory {
-    
+
+    static String first_variable;
+    static String operator;
+    static String second_variable;
+
+    public void set_first_variable(String first_variable) {
+        Memory.first_variable = first_variable;
+    }
+
+    public String get_first_variable() {
+        return first_variable;
+    }
+
+    public void set_operator(String operator) {
+        Memory.operator = operator;
+    }
+
+    public String get_operator() {
+        return operator;
+    }
+
+    public void set_second_variable(String second_variable) {
+        Memory.second_variable = second_variable;
+    }
+
+    public String get_second_variable() {
+        return second_variable;
+    }
+
+    public void main(String[] args) {
+        Memory memory = new Memory();
+
+        memory.set_first_variable("S");
+        System.out.println(memory.get_first_variable());
+
+    }
+
 }
