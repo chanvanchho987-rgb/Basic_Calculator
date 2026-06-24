99ece1fc36ae2dbe024e76830ab62d5e75a87d04
sva <chanvanchho987@gmail.com>
2026-06-24 15:28:45 +0700

    u

--- Controller.java
+++ Controller.java
@@ -1,9 +1,18 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private Button button_addition;
@@ -63,7 +72,121 @@ public class Controller {
     private Label label_result;
 
     @FXML
+    void on_backspace(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_clear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_divide(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_dot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_eigth(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_equal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_five(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_four(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_minus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_multiply(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_nine(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_one(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_plus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_seven(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_six(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_three(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_two(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_zero(ActionEvent event) {
+
+    }
+
+    @FXML
     void show_result(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert button_addition != null : "fx:id=\"button_addition\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_backspace != null : "fx:id=\"button_backspace\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_clear != null : "fx:id=\"button_clear\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_division != null : "fx:id=\"button_division\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_eight != null : "fx:id=\"button_eight\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_equal != null : "fx:id=\"button_equal\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_five != null : "fx:id=\"button_five\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_four != null : "fx:id=\"button_four\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_minus != null : "fx:id=\"button_minus\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_multiplication != null : "fx:id=\"button_multiplication\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_nine != null : "fx:id=\"button_nine\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_one != null : "fx:id=\"button_one\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_point != null : "fx:id=\"button_point\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_seven != null : "fx:id=\"button_seven\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_six != null : "fx:id=\"button_six\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_tree != null : "fx:id=\"button_tree\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_two != null : "fx:id=\"button_two\" was not injected: check your FXML file 'View.fxml'.";
+        assert button_zero != null : "fx:id=\"button_zero\" was not injected: check your FXML file 'View.fxml'.";
+        assert label_result != null : "fx:id=\"label_result\" was not injected: check your FXML file 'View.fxml'.";
 
     }
 
